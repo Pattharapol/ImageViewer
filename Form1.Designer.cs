713a181fd619abf6f2c615e174ff331598721ab6
tjtik83@gmail.com <tjtik83@gmail.com>
2021-07-23 10:12:13 +0700

    completed

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace ImageViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listViewFile = new System.Windows.Forms.ListView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // listViewFile
+            // 
+            this.listViewFile.HideSelection = false;
+            this.listViewFile.LargeImageList = this.imageList;
+            this.listViewFile.Location = new System.Drawing.Point(12, 12);
+            this.listViewFile.Name = "listViewFile";
+            this.listViewFile.Size = new System.Drawing.Size(579, 384);
+            this.listViewFile.SmallImageList = this.imageList;
+            this.listViewFile.TabIndex = 0;
+            this.listViewFile.UseCompatibleStateImageBehavior = false;
+            this.listViewFile.ItemActivate += new System.EventHandler(this.listViewFile_ItemActivate);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(498, 402);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(93, 36);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "&Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "88817_file_jpeg_icon.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(603, 450);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.listViewFile);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Viewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewFile;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ImageList imageList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class Form1 : Form
+    {
+        private List<string> fileNames = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "JPEG PNG JPG |*.jpeg;*.jpg;*.png";
+            ofd.ValidateNames = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileNames.Clear();
+                listViewFile.Clear();
+                foreach (string fileName in ofd.FileNames)
+                {
+                    FileInfo fi = new FileInfo(fileName);
+                    fileNames.Add(fi.FullName);
+                    listViewFile.Items.Add(fi.Name, 0);
+                }
+            }
+        }
+
+        private void listViewFile_ItemActivate(object sender, EventArgs e)
+        {
+            if (listViewFile.FocusedItem != null)
+            {
+                frmViewer frm = new frmViewer();
+                Image img = Image.FromFile(fileNames[listViewFile.FocusedItem.Index]);
+                frm.ImageBox = img;
+                frm.ShowDialog();
+            }
+        }
+    }
+}
